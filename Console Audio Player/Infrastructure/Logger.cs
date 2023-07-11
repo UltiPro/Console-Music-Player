67cd0f9d5fdd0314319fd06ebfe6d4061fd68944
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-07-11 20:05:07 +0200

    Correct Logger Location

--- Console Audio Player/Infrastructure/Logger.cs
+++ Console Audio Player/Infrastructure/Logger.cs
@@ -4,7 +4,7 @@ public class Logger
 {
     public static async void SaveLog(string info)
     {
-        using (StreamWriter file = new("AppLogs.txt", true))
+        using (StreamWriter file = new(AppContext.BaseDirectory + "/Console Music Player Logs.txt", true))
         {
             await file.WriteLineAsync(DateTime.Now + " " + info);
             file.Close();
