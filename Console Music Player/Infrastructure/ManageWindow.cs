200fe1c8de3bed2ddb9a1d1949e7a0f2292e0cff
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-08-09 20:30:01 +0200

    Update ManageWindow.cs

--- Console Music Player/Infrastructure/ManageWindow.cs
+++ Console Music Player/Infrastructure/ManageWindow.cs
@@ -5,16 +5,16 @@ namespace ManageWindowTool;
 
 class ManageWindow
 {
-    private const int MF_BYCOMMAND = 0x00000000;
     private const int SC_CLOSE = 0xF060;
     private const int SC_MAXIMIZE = 0xF030;
     private const int SC_SIZE = 0xF000;
-    [DllImport("user32.dll")]
-    private static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
-    [DllImport("user32.dll")]
-    private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+    private const int MF_BYCOMMAND = 0x00000000;
     [DllImport("kernel32.dll", ExactSpelling = true)]
     private static extern IntPtr GetConsoleWindow();
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetSystemMenu(IntPtr hWindow, bool bRevert);
+    [DllImport("user32.dll")]
+    private static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
     public static void Start()
     {
         try
